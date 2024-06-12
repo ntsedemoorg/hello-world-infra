d7012749ec8db4e691422d7ca351b8b0994a6448
Sam Ainsworth <sam.ainsworth@ukhsa.gov.uk>
2024-06-12 23:33:18 +0100

    correct target type

--- main.tf
+++ main.tf
@@ -80,6 +80,7 @@ resource "aws_lb_target_group" "frontend_tg" {
   port     = 5000
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+  target_type = "ip"
 
   health_check {
     interval            = 30
