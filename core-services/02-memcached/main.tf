e7d7384514b4dd1a1a773b00db8d1479b0c080fa
Sam Ainsworth <sam.ainsworth@ukhsa.gov.uk>
2024-06-13 14:54:12 +0100

    correct pg name

--- core-services/02-memcached/main.tf
+++ core-services/02-memcached/main.tf
@@ -3,7 +3,7 @@ resource "aws_elasticache_cluster" "memcached" {
   engine               = "memcached"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.memcached1.4"
+  parameter_group_name = "default.memcached1.6"
   port                 = 11211
 }
 
